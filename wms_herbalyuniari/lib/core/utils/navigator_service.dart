dfa61476c78d6a1e707f25ea01d88b0cc191f162
Fitri Awaliyah Muslim <150066815+Antariksaa12@users.noreply.github.com>
2024-05-28 21:23:23 +0700

    update navigator utils

--- wms_herbalyuniari/lib/core/utils/navigator_service.dart
+++ wms_herbalyuniari/lib/core/utils/navigator_service.dart
@@ -4,5 +4,33 @@ import '../app_export.dart';
 class NavigatorService {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-  
+  static Future<dynamic> pushNamed(
+    String routeName, {
+      dynamic arguments,
+    }) async {
+      return navigatorKey.currentState
+        ?.pushNamed(routeName, argument: arguments);
+    }
+
+    static void goBack() {
+      return navigatorKey.currentState?.pop();
+    }
+
+    static Future<dynamic> pushNamedAndRemoveUntil(
+      String routeName, {
+      bool routePredicate = false,
+      dynamic arguments,
+      }) async {
+        return navigatorKey.currentState?.pushNamedAndRemoveUntil(
+          routeName, (route) => routePredicate,
+          arguments: arguments);
+      }
+
+      static Future<dynamic> popAndPushNamed(
+        String routeName, {
+          dynamic arguments,
+        }) async {
+          return navigatorKey.currentState
+            ?.popAndPushNamed(routeName, arguments: arguments);
+        }
 }
